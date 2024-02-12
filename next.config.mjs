342f2c58522de1599195a1bbaaeb7580e4847b25
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-02-12 22:31:48 +0530

    placeholder img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: 'placehold.it',
+      port: '',
+      pathname: '/**',
+    }],
+  }
+};
 
 export default nextConfig;
