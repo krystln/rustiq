4d2f4679f3db874b30fbd9005562dcb34f90e248
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-02-12 22:34:44 +0530

    /collections

---
+++ src/app/collections/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Collection from "@/components/collection";
+
+const Collections = async () => {
+	return (
+		<div>
+			<Collection></Collection>
+		</div>
+	);
+};
+
+export default Collections;
