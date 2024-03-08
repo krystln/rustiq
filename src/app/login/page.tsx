71091999523552935d974ab20edf2ad6ee89de6e
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-08 13:02:40 +0530

    login page created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/auth-forms";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <AuthForm />
+    </div>
+  );
+};
+
+export default Page;
