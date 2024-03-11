80b3dc7446b92ab4d58623aee6b4bf11eab50d08
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-12 00:24:15 +0530

    stream-lined header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,85 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import Navbar from "./navbar";
-
-import { Input } from "./ui/input";
-import { Form, FormControl, FormField, FormItem } from "./ui/form";
-
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-import { useForm } from "react-hook-form";
-import { debounce } from "@/lib/utils";
-
-const Searchbar = () => {
-  const formSchema = z.object({
-    search: z.string().min(3),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      search: "",
-    },
-  });
-
-  useEffect(() => {
-    if (form.getValues("search") === "") return;
-
-    const [search, clearSearch] = debounce(
-      () => alert(form.getValues("search")),
-      1000,
-    );
-    search();
-
-    return () => {
-      clearSearch();
-    };
-  }, [form.getValues("search")]);
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit((formData) =>
-          alert(JSON.stringify(formData)),
-        )}
-      >
-        <FormField
-          control={form.control}
-          name="search"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input {...field} placeholder="Search" />
-              </FormControl>
-            </FormItem>
-          )}
-        ></FormField>
-      </form>
-    </Form>
-  );
-};
-
-const Profile = () => {
-  return (
-    <div>
-      <img src="https://via.placeholder.com/50" alt="Profile" />
-      {/* <Image
-        src="https://via.placeholder.com/50"
-        alt="profile"
-        height={50}
-        width={50}
-      ></Image> */}
-    </div>
-  );
-};
+import Navbar from "./nav-bar";
+import Searchbar from "./search-bar";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const session = false;
 
   return (
     <div className="flex items-center justify-center gap-4 p-2">
-      <h1 className="text-xl font-bold">Rustiq</h1>
+      <Link href="/">
+        <Button variant="link">
+          <h1 className="text-xl font-bold">Rustiq</h1>
+        </Button>
+      </Link>
       <Navbar />
       <Searchbar />
     </div>
