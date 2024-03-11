cfe91a5eba6d8de6f9f94f4ffa7697a773c14729
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-12 00:15:47 +0530

    header test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/header";
+
 export default function Home() {
-	return (
-		<main>
-			<div>Test</div>
-		</main>
-	);
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
