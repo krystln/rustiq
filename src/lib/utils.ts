0711d3f8f6aef0e037676aa3d728400e13b5d45e
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-12 00:11:33 +0530

    debounce util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number,
+) {
+  let timer: NodeJS.Timeout;
+  return [
+    (...args: Params) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, timeout);
+    },
+    () => clearTimeout(timer),
+  ];
 }
