e01ed3e80dc588066804cf89bef81211a8d345a2
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-26 13:24:52 +0530

    temp product card

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,19 +1,46 @@
-// type Product = any;
-
-async function fetcher(id: number) {
-  const res = await fetch(`http://localhost:3000/api/v0.1/products/${id}`);
-
-  if (!res.ok) {
-    throw new Error("An error occurred while fetching the data");
-  }
-
-  console.log(id);
-  return await res.json();
-}
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 const ProductCard = async ({ id }: { id: number }) => {
-  const product = await fetcher(id);
-  return <div>{JSON.stringify(product)}</div>;
+  let product = null;
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000//api/v0.1/products/${id}`,
+    )
+      .then((res) => (res.status === 200 ? res.json() : null))
+      .catch((e) => console.error(e));
+    product = response;
+  } catch (e) {
+    console.error(e);
+  }
+
+  return (
+    <Card className="h-98 w-56 rounded-sm p-6">
+      <Image
+        src="http://via.placeholder.com/180x210"
+        alt="placeholder"
+        width={180}
+        height={210}
+      />
+      <CardTitle className="flex items-center justify-between py-2 text-xl">
+        <div>{product.name}</div>
+        <Button variant="outline" className="px-2">
+          <span className="material-symbols-outlined">shopping_cart</span>
+        </Button>
+      </CardTitle>
+      <CardContent className="flex flex-wrap gap-2 p-0">
+        {["test", "test2", "test3", "test4"].map((tag: string) => (
+          <Badge key={tag} className="">
+            {tag}
+          </Badge>
+        ))}
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ProductCard;
