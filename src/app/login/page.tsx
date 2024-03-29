521bb8b10ba8eb74edfa384212502433597b72ac
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-29 17:10:45 +0530

    extra space removed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "@/components/auth-forms";
 
 const Page = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex min-h-[calc(100vh-100px)] w-full items-center justify-center">
       <AuthForm />
     </div>
   );
