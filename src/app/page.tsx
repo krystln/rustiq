5fb02fa7ab0223f483fd5cd77b30dd772a6a019a
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-03-31 22:46:34 +0530

    temp login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ProductCard from "@/components/product-card";
 import Buttons from "@/components/buttons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <Buttons />
+      <Link href="/login">Login</Link>
       <ProductCard id={1} />
     </main>
   );
