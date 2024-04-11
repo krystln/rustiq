62d014cf67cabc8e2a786983bf9c32529842d158
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-11 16:49:40 +0530

    auth/google better look

--- src/app/auth/google/page.tsx
+++ src/app/auth/google/page.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Page = () => {
-  return <div>Google Auth</div>;
+  return (
+    <div className="mt-52 flex w-full items-center justify-center font-mono text-lg">
+      {"Stuck?? "}
+      <Link href="/">
+        <Button variant="link" className="text-lg">
+          Get Back!!
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Page;
