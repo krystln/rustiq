be22662e05549ed3b59aea5435bc61565b1a8e26
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-12 20:22:40 +0530

    modal above everything

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + "flex min-h-screen"}>
-        <Header />
         <div className="h-full">{data}</div>
+        <Header />
         {children}
         <SpeedInsights />
         <Analytics />
