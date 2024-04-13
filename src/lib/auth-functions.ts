57ab4fc5e0f70ff9ebb4b5ded0de92f35ffe2d1d
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-13 21:04:07 +0530

    login flow with data to db

--- src/lib/auth-functions.ts
+++ src/lib/auth-functions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { createUser } from "@/supabse/handler";
 import { cookies } from "next/headers";
 
 const fetchUserInfo = async (access_token: string) => {
@@ -18,7 +19,15 @@ const fetchUserInfo = async (access_token: string) => {
     });
 
   // console.log("Cookies set with user data : ", JSON.stringify(await userData));
+  const error = await createUser({
+    userData: {
+      name: userData.name,
+      email: userData.email,
+      image: userData.picture,
+    },
+  });
   cookies().set("user", JSON.stringify(await userData));
+  if (error === null) console.log("Unable to save user data to database");
 };
 
 export default fetchUserInfo;
