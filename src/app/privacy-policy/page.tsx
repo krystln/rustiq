a3e2a2baabd9ffe059434d2ecfda58309cc3176e
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-13 21:22:51 +0530

    temp terms and privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>{`Information Collected :
+            1.Your Name
+            2.Your Email Address
+            3.Link to your profile picture
+
+    How We Use Your Information
+
+    We use this information to authorize your access to our services, to communicate with you, and to improve our services.
+    
+    Protection of Your Information
+    
+    We take the security and protection of your information seriously. We have implemented appropriate technical and organizational measures to safeguard your data against unauthorized access, alteration, disclosure, or destruction.
+    Sharing Your Information
+    
+    We do not sell, trade, or otherwise transfer your personal information to outside parties. Your information is only accessible to authorized personnel within our organization for the purposes mentioned above.
+    Opting Out
+    
+    Cookies
+    
+    Our website may uses cookies to authorize access to service in the Website. These cookies do not collect personal information and are used for functional purposes only.
+
+    Third-Party Links
+    
+    Our website may contain links to third-party websites. We are not responsible for the privacy practices or content of these external sites. We encourage you to review the privacy policies of those websites.
+    Changes to this Privacy Policy
+    
+    We may update this privacy policy to reflect changes in our practices or for legal reasons. The most recent version of the privacy policy will always be available on our website.
+
+    Contact Us
+    
+    If you have any questions or concerns about our privacy policy or how we handle your personal information, please contact us at akshitgoyal.10000@gmail.com.
+    
+    `}</div>
+  );
+};
+
+export default Page;
---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>{`Terms of Service
+
+      These Terms of Service ("Terms") govern your access to and use of [Rustiq] ("the Service"), and any information, text, graphics, photos or other materials uploaded, downloaded or appearing on the Service (collectively referred to as "Content"). Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. By accessing or using the Service you agree to be bound by these Terms.
+      
+      Basic Terms
+      
+          1.You are responsible for your use of the Service and any Content you provide.
+          2.You must be at least 13 years old to use the Service.
+          3.You must not abuse, harass, threaten, impersonate or intimidate other users of the Service.
+          4.You may not use the Service for any illegal or unauthorized purpose. International users agree to comply with all local laws regarding online conduct and acceptable content.
+          5.You are solely responsible for your conduct and any data, text, information, screen names, graphics, photos, profiles, audio and video clips, links ("Content") that you submit, post, and display on the Service.
+      
+      Intellectual Property Rights
+      
+          1.The Service and its original content (excluding Content provided by users), features and functionality are and will remain the exclusive property of [Rustiq] and its licensors.
+          2.The Service is protected by copyright, trademark, and other laws of both India and foreign countries. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of [Rustiq].
+      
+      Termination
+      
+          We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.
+          All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+      
+      Changes
+      
+          We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will try to provide at least 30 days' notice prior to any new terms taking effect.
+          What constitutes a material change will be determined at our sole discretion.
+      
+      Contact Us
+      
+      If you have any questions about these Terms, please contact us at akshitgoyal.10000@gmail.com.`}</div>
+  );
+};
+
+export default Page;
