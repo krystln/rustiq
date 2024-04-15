2c755c9d787aec927d1201cec9142ed040b6680a
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-15 10:38:23 +0530

    email/password verification added

--- src/app/api/v0.1/auth/route.ts
+++ src/app/api/v0.1/auth/route.ts
@@ -1,40 +1,39 @@
 "use server";
 
-import { cookies } from "next/headers";
+import { verifyLogin } from "@/supabse/handler";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
-function verifyLogin(email: string, password: string) {
-  console.log(email, password);
-  return true;
-}
 
 const POST = async (req: NextRequest, res: NextResponse) => {
   try {
     const body = await req.json();
     const { email, password } = body;
 
-    if (verifyLogin(email, password)) {
-      // console.log("Redirecting to /");
-      const temp = cookies().set(
-        "currentUser",
-        JSON.stringify({ email, name: "John Doe" }),
-        {
-          expires: Date.now() + 1000 * 60 * 5,
-        },
+    const password_hash = await crypto.subtle
+      .digest("SHA-256", new TextEncoder().encode(password))
+      .then((hash) =>
+        Array.from(new Uint8Array(hash))
+          .map((b) => b.toString(16).padStart(2, "0"))
+          .join(""),
       );
 
-      console.log(temp);
+    // console.log({ email, password_hash });
 
-      return new NextResponse("Logged in", { status: 200 });
-    } else {
-      return new NextResponse("Failed to login", { status: 401 });
+    const verification = await verifyLogin(email, password_hash);
+    switch (verification.status) {
+      case 200:
+        return new NextResponse("Logged in", { status: 200 });
+      case 401:
+        return new NextResponse("Failed to login", { status: 401 });
+      case 404:
+        return new NextResponse("User not found", { status: 404 });
+      default:
+        throw new Error("Database error occured");
     }
   } catch (e) {
     console.error(e);
     return new NextResponse("Internal error occured", { status: 500 });
   }
-  // NextResponse.redirect("/");
 };
 
 export { POST };
--- src/supabse/handler.ts
+++ src/supabse/handler.ts
@@ -15,3 +15,25 @@ export const createUser = async ({ userData }: { userData: Insert }) => {
   // console.log(error);
   return error;
 };
+
+export const verifyLogin = async (email: string, password: string) => {
+  const { data: User, error } = await supabase
+    .from("User")
+    .select("password_hash")
+    .eq("email", email);
+  if (error) {
+    console.error(error);
+    return {
+      status: 500,
+    };
+  }
+
+  if (User[0].password_hash === null) {
+    return {
+      status: 404,
+    };
+  }
+  return {
+    status: User?.[0]?.password_hash === password ? 200 : 401,
+  };
+};
