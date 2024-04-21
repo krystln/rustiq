f85fbfd9c250ad23ea60d1c6045391a5a0158511
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-04-21 07:19:33 +0530

    better modal

--- src/app/@data/(.)products/[product_name]/page.tsx
+++ src/app/@data/(.)products/[product_name]/page.tsx
@@ -4,12 +4,20 @@ import { Product } from "@/components/product";
 import { Button } from "@/components/ui/button";
 import GoogleIcon from "@/components/utility";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Page = ({ params }: { params: { product_name: string } }) => {
   const router = useRouter();
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
-    <div className="absolute z-50 flex h-full w-full justify-center overflow-clip bg-[#00000088]">
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-gray-900 bg-opacity-50">
       <div className="my-10 flex w-2/3 flex-col items-start overflow-y-auto overflow-x-clip border border-black bg-white p-4">
         <Button onClick={() => router.back()} variant="ghost" className="">
           <GoogleIcon className="scale-125">arrow_back</GoogleIcon>
