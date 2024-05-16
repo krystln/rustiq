dd4d39e2494964caf195b9316eae18c8da2e70b6
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-05-16 15:50:35 +0530

    profile uses new session

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,23 +1,26 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import GoogleIcon from "@/components/utility";
-import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-  if (!cookies().get("user")) redirect("/login");
+const Page = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+  const userData = session.user!;
 
-  const userData = JSON.parse(cookies().get("user")?.value!);
   return (
     <div className="mt-10 flex w-full justify-center">
       <div className="center flex w-2/3 flex-col items-center gap-10">
         <h1 className="text-3xl font-bold">Your Profile</h1>
         <div className="flex items-center gap-4">
           <Image
-            src={userData.picture}
-            alt=""
+            src={userData.image || ""}
+            alt="Avatar"
             width={100}
             height={100}
             className="rounded-full border border-black"
