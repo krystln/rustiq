40284d7b1e9c195fdf2b256ad38e2825d118ba14
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-05-17 19:39:04 +0530

    removed credential auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
 export default async function Home() {
   return (
-    <main>
-      <div className="flex flex-wrap items-center justify-center gap-12">
-        <div>
-          <h1 className="text-4xl font-bold">Hello, world!</h1>
-          <p className="text-lg text-gray-600">
-            This is a Next.js with Firebase template.
-          </p>
-        </div>
-        <div>
-          <img
-            src="https://img.icons8.com/color/452/firebase.png"
-            alt="Firebase"
-            className="h-48 w-48"
-          />
-        </div>
+    <main className="h-full">
+      <div className="flex h-full flex-wrap items-center justify-center gap-12">
+        <div className="h-[450px] w-1/2 bg-[#efefef]"></div>
       </div>
     </main>
   );
--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google,
-    Github,
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        return {
-          id: "23",
-          email: "credentials.email",
-          password: "credentials.password",
-        };
-      },
-    }),
-  ],
+  providers: [Google, Github],
 });
