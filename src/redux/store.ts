f03bdfbf3d725fe30ee6014b3fd310d599ec7b14
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-05-28 08:45:54 +0530

    implemented persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,27 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import profileReducers from "./feature/profileSlice";
+import cartSlice from "./feature/cart/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const rootReducer = combineReducers({ profileReducers });
-
-export const makeStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
+const persistConfig = {
+  key: "root",
+  storage,
 };
 
+const rootReducer = combineReducers({ profileReducers, cartSlice });
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducers,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
+
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>;
+export type AppStore = typeof store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
+export type RootState = AppStore["getState"];
 export type AppDispatch = AppStore["dispatch"];
