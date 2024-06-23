71cec10e32481c540ed4d4925f62636d7226491a
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-06-24 00:28:21 +0530

    colorful title!

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,17 +3,25 @@
 import Navbar from "./nav-bar";
 import Searchbar from "../search-bar";
 import Link from "next/link";
-import { Button } from "../ui/button";
 import { auth } from "@/auth";
 
 const Header = async () => {
   const session = await auth();
   return (
     <div className="flex items-center justify-center gap-4 p-2">
-      <Link href="/">
-        <Button variant="link">
-          <h1 className="text-xl font-bold">Rustiq</h1>
-        </Button>
+      <Link href="/" className="font-['Inika'] text-2xl font-bold">
+        <div className="group inline w-fit">
+          <span className="absolute -z-10 h-10 w-10 rotate-12 group-hover:bg-orange-500" />
+          Ru
+        </div>
+        <div className="group inline w-fit">
+          <span className="absolute -z-10 h-10 w-10 -rotate-12 group-hover:bg-green-400" />
+          st
+        </div>
+        <div className="group inline w-fit">
+          <span className="absolute -z-10 h-10 w-10 rotate-12 group-hover:bg-pink-400" />
+          iQ
+        </div>
       </Link>
       <Navbar session={session} />
       <Searchbar />
